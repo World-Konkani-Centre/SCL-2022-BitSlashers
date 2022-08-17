63aa66a48bba8180581c68fb9720dff0e2e0a869
Rakshitha Nayak <rakshitharnayak13@gmail.com>
2022-08-17 23:15:51 +0530

    Added language translator page

--- SCL_Project/static/WeGrow/js/eng.js
+++ SCL_Project/static/WeGrow/js/eng.js
@@ -7,6 +7,94 @@ var eng=[
    "Analytics",//5
    "LogOut",//6
    "Trade",//7
+   //index
    "A single stop solution",//8
-  
+  "Your Digital Marketplace where fair crop trade made EASY, FAST and TRANSPARENT!",//9
+  "Search" ,//10
+  "Clients", //11
+  "Projects" ,//12
+  "Support", //13
+  "Workers", //14
+  "About Us",//15
+   "We drive agricultural transactions through our digital platform. We help farmers to meet consumers, aided with analytical insights and regular price visibility", //16
+"Easy",//17
+"Trustworthy", //18
+"User-Friendly", //19
+
+//sell
+"Sell a product", //20
+"Selling is Happiness", //21
+"Sell Product", //22
+"Location Details", //23
+"Loading", //24
+"Could not place offer! Try again!", //25
+"Your offer has been successfully placed!", //26
+"OFFER", //27
+
+//analytics_option
+"Tell us your choice!",//28
+"SOLD",//29
+"BOUGHT" ,//30
+"IN SALE",//31
+
+//contact
+"Contact" ,//32
+"Connect to us! We are happy to help :)",//33
+"Location:",//34
+"Sushiksha",//35
+"Email:",//36
+"bitslashers@gmail.com",//37
+"Call:",//38
+"+91 12345 67890",//39
+"Send Message",//40
+
+
+//profile
+
+"Account Settings",//41
+"Edit Profile" ,//42
+"Your Personal Details",//43
+"Save Profile",//44
+
+//option
+"Tell us your choice! ",//45
+"SELL",//46
+"BUY",//47
+
+//buy
+"Buy a product",//48
+"Happiness is not a goal; it is a by-product." ,//49
+"Buy Product",//50
+"Location Details",//51
+"SEARCH",//52
+
+          
+//analytics_buyer
+"Bought",//53
+"Happiness is not a goal; it is a by-product.",//54
+
+//analytics_bought     //analytics_forSale
+"In Sale",//55
+"Happiness is not a goal; it is a by-product." ,//56
+
+//analytics_seller
+"Sold" , //57
+"Happiness is not a goal; it is a by-product." ,//58
+
+//pricetrend
+"What's out there?",//59
+"Check the price trend" //60
+
+
+
+
+
+// "",//67
+// "",//68
+
+
+
+
+
+
   ]
