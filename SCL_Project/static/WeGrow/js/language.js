0e16e8ab38cd707bae900767787e3dd2f62445d3
kptriescoding <karthikpai0*@gmail.com>
2022-08-26 21:30:30 +0530

    Language Feature Update

--- SCL_Project/static/WeGrow/js/language.js
+++ SCL_Project/static/WeGrow/js/language.js
@@ -23,15 +23,49 @@
        if(document.getElementById(i))
        document.getElementById(i).innerText=eng[i];
      }
+     if(document.getElementById("video1")){
+     if(document.getElementById("video2"))document.getElementById("video2").remove();
+      var parent = document.getElementById('video1');
+      var customElement = createCustomElement("https://youtu.be/cfTRou4JtW8","glightbox play-btn video2");
+      parent.appendChild(customElement);
+      console.log(document.getElementById("video2"))
+     }
+      return;
    }
-     else if(l==="Kannada")
+     else if(l==="Kannada"){
      for(var i=0;i<100;i++){
      if(document.getElementById(i))
      document.getElementById(i).innerText=kan[i];
      }
-     else if(l==="Konkani")
+     if(document.getElementById("video1")){
+      if(document.getElementById("video2"))document.getElementById("video2").remove();
+      var parent = document.getElementById('video1');
+      var customElement = createCustomElement("https://youtu.be/LIvm_GfcTWo","glightbox play-btn");
+      parent.appendChild(customElement);
+      return;
+     }
+     }
+     else if(l==="Konkani"){
      for(var i=0;i<100;i++){
      if(document.getElementById(i))
      document.getElementById(i).innerText=konkani[i];
      }
+     if(document.getElementById("video1")){
+      if(document.getElementById("video2"))document.getElementById("video2").remove();
+      var parent = document.getElementById('video1');
+      var customElement = createCustomElement("https://youtu.be/S7GDda309JU","glightbox play-btn");
+      parent.appendChild(customElement);
+      return;
+     }
+     } 
    }
+   function createCustomElement( anchorLink,anchorClass){
+    var aTag = document.createElement("a");
+    aTag.href = anchorLink;
+    const myArray = anchorClass.split(" ");
+    for(var i=0;i<2;i++){
+    aTag.classList.add(myArray[i]);
+    }
+    aTag.setAttribute("id","video2")
+    return aTag ;
+  }
